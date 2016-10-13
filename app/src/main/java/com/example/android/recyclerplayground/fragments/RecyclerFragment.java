8eb058853dcd8f4ae53b2dabe3b70a3d40ad2062
Mehmet Kologlu <m.kologlu@vispera.co>
2016-10-13 18:05:17 +0300

    Change R.id. switch to if statement as required by library

--- app/src/main/java/com/example/android/recyclerplayground/fragments/RecyclerFragment.java
+++ app/src/main/java/com/example/android/recyclerplayground/fragments/RecyclerFragment.java
@@ -16,108 +16,105 @@ import com.example.android.recyclerplayground.NumberPickerDialog;
 import com.example.android.recyclerplayground.R;
 import com.example.android.recyclerplayground.adapters.SimpleAdapter;
 
+
 public abstract class RecyclerFragment extends Fragment implements AdapterView.OnItemClickListener {
 
-    private RecyclerView mList;
-    private SimpleAdapter mAdapter;
+  private RecyclerView mList;
+  private SimpleAdapter mAdapter;
 
-    /** Required Overrides for Sample Fragments */
+  /**
+   * Required Overrides for Sample Fragments
+   */
 
-    protected abstract RecyclerView.LayoutManager getLayoutManager();
-    protected abstract RecyclerView.ItemDecoration getItemDecoration();
-    protected abstract int getDefaultItemCount();
-    protected abstract SimpleAdapter getAdapter();
+  protected abstract RecyclerView.LayoutManager getLayoutManager();
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-    }
+  protected abstract RecyclerView.ItemDecoration getItemDecoration();
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_recycler, container, false);
+  protected abstract int getDefaultItemCount();
 
-        mList = (RecyclerView) rootView.findViewById(R.id.section_list);
-        mList.setLayoutManager(getLayoutManager());
-        mList.addItemDecoration(getItemDecoration());
+  protected abstract SimpleAdapter getAdapter();
 
-        mList.getItemAnimator().setAddDuration(1000);
-        mList.getItemAnimator().setChangeDuration(1000);
-        mList.getItemAnimator().setMoveDuration(1000);
-        mList.getItemAnimator().setRemoveDuration(1000);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setHasOptionsMenu(true);
+  }
 
-        mAdapter = getAdapter();
-        mAdapter.setItemCount(getDefaultItemCount());
-        mAdapter.setOnItemClickListener(this);
-        mList.setAdapter(mAdapter);
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View rootView = inflater.inflate(R.layout.fragment_recycler, container, false);
 
-        return rootView;
-    }
+    mList = (RecyclerView) rootView.findViewById(R.id.section_list);
+    mList.setLayoutManager(getLayoutManager());
+    mList.addItemDecoration(getItemDecoration());
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.grid_options, menu);
-    }
+    mList.getItemAnimator().setAddDuration(1000);
+    mList.getItemAnimator().setChangeDuration(1000);
+    mList.getItemAnimator().setMoveDuration(1000);
+    mList.getItemAnimator().setRemoveDuration(1000);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        NumberPickerDialog dialog;
-        switch (item.getItemId()) {
-            case R.id.action_add:
-                dialog = new NumberPickerDialog(getActivity());
-                dialog.setTitle("Position to Add");
-                dialog.setPickerRange(0, mAdapter.getItemCount());
-                dialog.setOnNumberSelectedListener(new NumberPickerDialog.OnNumberSelectedListener() {
-                    @Override
-                    public void onNumberSelected(int value) {
-                        mAdapter.addItem(value);
-                    }
-                });
-                dialog.show();
+    mAdapter = getAdapter();
+    mAdapter.setItemCount(getDefaultItemCount());
+    mAdapter.setOnItemClickListener(this);
+    mList.setAdapter(mAdapter);
 
-                return true;
-            case R.id.action_remove:
-                dialog = new NumberPickerDialog(getActivity());
-                dialog.setTitle("Position to Remove");
-                dialog.setPickerRange(0, mAdapter.getItemCount()-1);
-                dialog.setOnNumberSelectedListener(new NumberPickerDialog.OnNumberSelectedListener() {
-                    @Override
-                    public void onNumberSelected(int value) {
-                        mAdapter.removeItem(value);
-                    }
-                });
-                dialog.show();
+    return rootView;
+  }
 
-                return true;
-            case R.id.action_empty:
-                mAdapter.setItemCount(0);
-                return true;
-            case R.id.action_small:
-                mAdapter.setItemCount(5);
-                return true;
-            case R.id.action_medium:
-                mAdapter.setItemCount(25);
-                return true;
-            case R.id.action_large:
-                mAdapter.setItemCount(196);
-                return true;
-            case R.id.action_scroll_zero:
-                mList.scrollToPosition(0);
-                return true;
-            case R.id.action_smooth_zero:
-                mList.smoothScrollToPosition(0);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+  @Override
+  public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    inflater.inflate(R.menu.grid_options, menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    NumberPickerDialog dialog;
+    int itemId = item.getItemId();
+    if (itemId == R.id.action_add) {
+      dialog = new NumberPickerDialog(getActivity());
+      dialog.setTitle("Position to Add");
+      dialog.setPickerRange(0, mAdapter.getItemCount());
+      dialog.setOnNumberSelectedListener(new NumberPickerDialog.OnNumberSelectedListener() {
+        @Override
+        public void onNumberSelected(int value) {
+          mAdapter.addItem(value);
         }
+      });
+      dialog.show();
+    } else if (itemId == R.id.action_remove) {
+      dialog = new NumberPickerDialog(getActivity());
+      dialog.setTitle("Position to Remove");
+      dialog.setPickerRange(0, mAdapter.getItemCount() - 1);
+      dialog.setOnNumberSelectedListener(new NumberPickerDialog.OnNumberSelectedListener() {
+        @Override
+        public void onNumberSelected(int value) {
+          mAdapter.removeItem(value);
+        }
+      });
+      dialog.show();
+    } else if (itemId == R.id.action_empty) {
+      mAdapter.setItemCount(0);
+    } else if (itemId == R.id.action_small) {
+      mAdapter.setItemCount(5);
+    } else if (itemId == R.id.action_medium) {
+      mAdapter.setItemCount(25);
+    } else if (itemId == R.id.action_large) {
+      mAdapter.setItemCount(196);
+    } else if (itemId == R.id.action_scroll_zero) {
+      mList.scrollToPosition(0);
+    } else if (itemId == R.id.action_smooth_zero) {
+      mList.smoothScrollToPosition(0);
+    } else {
+      return super.onOptionsItemSelected(item);
     }
+    return true;
+  }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getActivity(),
-                "Clicked: " + position + ", index " + mList.indexOfChild(view),
-                Toast.LENGTH_SHORT).show();
-    }
+  @Override
+  public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    Toast.makeText(getActivity(),
+                   "Clicked: " + position + ", index " + mList.indexOfChild(view),
+                   Toast.LENGTH_SHORT).show();
+  }
 }
